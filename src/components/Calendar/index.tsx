19c095544159e99b18507524899d4b94697f393a
Olaobaju Abraham <olaobajua@gmail.com>
2024-12-26 18:37:37 +0000

    feat: company color switch

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,3 +1,4 @@
+import {useEffect, useRef} from 'react';
 import type {CalendarProps, DateValue} from 'react-aria-components';
 import {Button, Calendar, CalendarCell, CalendarGrid, Heading, Text} from 'react-aria-components';
 import classes from './style.module.css';
@@ -6,11 +7,41 @@ interface MyCalendarProps<T extends DateValue> extends CalendarProps<T> {
   errorMessage?: string;
 }
 
+const getRandomColor = () => {
+  // Generate a random hex color
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 export default function MyCalendar<T extends DateValue>(
   { errorMessage, ...props }: MyCalendarProps<T>
 ) {
+
+  const calendarRef = useRef(null);
+
+  useEffect(function() {
+    
+    const timeoutId = setTimeout(() => {}, 1000);
+
+    if(calendarRef.current) {
+      let htmlElement = calendarRef.current;
+      htmlElement.style.setProperty('--highlight-background', getRandomColor());
+      htmlElement.style.setProperty('--highlight-foreground', 'black');
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    }
+    
+  },[]);
+
+
   return (
-    <Calendar {...props} className={classes['react-aria-Calendar']}>
+    <Calendar {...props} ref={calendarRef}  className={classes['react-aria-Calendar']}>
       <header>
         <Button className={classes['react-aria-Button'] } slot="previous">◀</Button>
         <Heading className={classes['react-aria-Heading']} />
