6800334598cc8588cec7b0878e4b49a5392b726f
Olaobaju Abraham <olaobajua@gmail.com>
2025-01-18 12:56:09 +0000

    build static index.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index-B8IM7gcy.js'
+      }
+    }
+  },
   plugins: [react()],
 })
